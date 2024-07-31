f2039b11176fa438cb51903d2f9c390bf5f345af
João Pedro Miano <joaomiano15@gmail.com>
2024-07-30 22:19:12 -0300

    add atacks in function doAttack

--- assets/scripts/classes.js
+++ assets/scripts/classes.js
@@ -65,28 +65,62 @@ class Stage{
         this.figther2El = figther2El;
     }
 
+    //Inicia a luta ao clicar no botão "Atacar"
+
     start(){
         this.uptade();
 
         this.figther1El.querySelector('#attack_button').addEventListener('click', () => this.doAttack(this.figther1, this.figther2));
         this.figther2El.querySelector('#attack_button').addEventListener('click', () => this.doAttack(this.figther2, this.figther1));
     }
+     
+    //Função que define o nome e atualiza a vida após o ataque
 
     uptade(){
         //Figther 1
-        this.figther1El.querySelector('.name').innerHTML= `${this.figther1.name} - ${this.figther1.life} HP`;
+        this.figther1El.querySelector('.name').innerHTML= `${this.figther1.name} - ${this.figther1.life.toFixed(2)} HP`;
         let pct1 = (this.figther1.life / this.figther1.maxLife) * 100;
         this.figther1El.querySelector('.life').style.width = `${pct1}%`;
         
         //Figther 2
-        this.figther2El.querySelector('.name').innerHTML= `${this.figther2.name} - ${this.figther2.life} HP`;
+        this.figther2El.querySelector('.name').innerHTML= `${this.figther2.name} - ${this.figther2.life.toFixed(2)} HP`;
         let pct2 = (this.figther2.life / this.figther2.maxLife) * 100;
         this.figther2El.querySelector('.life').style.width = `${pct2}%`;
 
     }
 
     doAttack(attacking, attacked){
-        console.log(`${attacking.name} está atacando ${attacked.name}`);
+
+        //Verificando se o atacante ou o atacado esta vivo
+
+        if (attacking.life <= 0){
+            console.log("Morto não ataca!");
+            return;
+        }else if(attacked.life <= 0){
+            console.log("Chutando cachorro morto!")
+            return;
+        }
+
+        //Definindo uma valor aleatorio para multiplicar o dano e defesa
+
+        let factorAttack = (Math.random() * 2 ).toFixed(2);
+        let factorDefese = (Math.random() * 2 ).toFixed(2);
+
+        //Definindo o dano atual do atacante e defesa do atacado
+
+        let actualAttack = factorAttack * attacking.attack;
+        let actualDefese = factorDefese * attacked.defese;
+    
+        //Verifica se o atacante conseguiu causar dano e se sim atualiza a vida do atacado
+
+        if(actualAttack > actualDefese){
+            attacked.life -= actualAttack;
+
+            console.log(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano no ${attacked.name}`)
+        } else{
+            console.log(`${attacked.name} conseguiu defender`)
+        }
+
 
 
         this.uptade();
